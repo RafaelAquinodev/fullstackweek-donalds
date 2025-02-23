7a237a2ff4e823a190eb3c84a2c6cb5d5770a5e1
RafaelAquinodev <oaquinorafael@gmail.com>
2025-02-23 11:21:48 -0300

    feat: adicionando setup inicial do shadcn e components button e input

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500"></h1>
+    </div>
+  );
+};
+
+export default ProductPage;
