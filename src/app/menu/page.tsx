fb804972e277a6bdb4c5b31958af723bb7aa4d45
RafaelAquinodev <oaquinorafael@gmail.com>
2025-02-24 18:49:51 -0300

    feat: adicionando pagina menu

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,8 @@
+interface RestaurantMenuPageProps {
+    params: Promise<{slug: string}>
+}
+const RestaurantMenuPage = async ({params}:RestaurantMenuPageProps) => {
+    return();
+}
+
+export default RestaurantMenuPage;
